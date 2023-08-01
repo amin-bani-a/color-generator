25a078138f68fce7680500c5fc9faa8770c9f391
amin-bani-a <amin.baniamerian67@gmail.com>
2023-08-01 21:18:39 +0400

    add tostify

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,19 @@
+import { useState } from "react";
+import ColorList from "./ColorList";
+import Form from "./Form";
+import Values from "values.js";
+import { ToastContainer, toast } from "react-toastify";
+
 const App = () => {
-  return <h2>Color Generator Starter</h2>;
+  const [colors, setColors] = useState(new Values("#2c4daf").all(10));
+  toast.error("error");
+  toast.success("success");
+  return (
+    <main>
+      <Form />
+      <ColorList colors={colors} />
+      <ToastContainer position="top-center" />
+    </main>
+  );
 };
 export default App;
